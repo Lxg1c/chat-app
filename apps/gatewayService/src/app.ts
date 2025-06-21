36151c2b7baac2513423f38c9464086e793f67ef
Lxg1c <teran_2005@mail.ru>
2025-06-21 18:23:39 +0300

    add gateway to other auth routes

--- apps/gatewayService/src/app.ts
+++ apps/gatewayService/src/app.ts
@@ -5,8 +5,18 @@ import {settings} from "./shared/config";
 
 const app = express();
 
+app.use(`${settings.api_v1_prefix}/register`, createProxyMiddleware({
+    target: "http://localhost:5001/api/v1/auth/register",
+    changeOrigin: true,
+}));
+
 app.use(`${settings.api_v1_prefix}/login`, createProxyMiddleware({
     target: "http://localhost:5001/api/v1/auth/login",
+    changeOrigin: true,
+}));
+
+app.use(`${settings.api_v1_prefix}/refresh`, createProxyMiddleware({
+    target: "http://localhost:5001/api/v1/auth/refresh",
     changeOrigin: true,
 }));
 
