45923f71645ed6344bf4cce5c7fafb2441e01156
Lxg1c <teran_2005@mail.ru>
2025-06-23 17:15:22 +0300

    add cookier parser

--- apps/authService/src/app.ts
+++ apps/authService/src/app.ts
@@ -4,12 +4,14 @@ import { logger } from "./shared/logger"
 import authRouter from "./routers/authRouter";
 import { settings } from "./shared/config";
 import cors from "cors";
-
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
 
 app.use(`${settings.api_v1_prefix}/auth`, authRouter);
 
