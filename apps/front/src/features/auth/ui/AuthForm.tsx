6bb1759cdfe345c153c3055dd4e91e03d5244e5e
Lxg1c <teran_2005@mail.ru>
2025-06-23 17:17:30 +0300

    refactored form

--- apps/front/src/features/auth/ui/AuthForm.tsx
+++ apps/front/src/features/auth/ui/AuthForm.tsx
@@ -1,69 +1,130 @@
-"use client";
+'use client';
 
-import {FormEvent, useState} from "react";
-import { login } from "../model/login";
-import {useUserStore} from "@/entities/user/model/store";
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { handleSignin, handleSignup } from '@/features/auth/model/api';
+import { FormType, FormValues } from '@/features/auth/model/models';
+import { useUserStore } from '@/entities/User/model/store';
+import { useRouter } from 'next/navigation';
+import Button from '@/shared/ui/button/Button';
 
-export const AuthForm = () => {
-    const [username, setUsername] = useState("admin");
-    const [password, setPassword] = useState("adminpassword");
+interface AuthFormProps {
+    type: FormType;
+    setFormType: (type: FormType) => void;
+}
+
+const AuthForm: React.FC<AuthFormProps> = ({ type, setFormType }) => {
+    const isSignup = type === 'signup';
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
     const setTokens = useUserStore(state => state.setTokens);
+    const router = useRouter();
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        const success = await login(username, password);
-        if (success) {
-            console.log(success);
-            setTokens(success.accessToken, success.refreshToken);
-        } else {
-            alert("❌ Login failed");
-        }
-    };
+    const formik = useFormik<FormValues>({
+        initialValues: {
+            username: '',
+            password: '',
+            phone: '',
+        },
+        validationSchema: Yup.object({
+            username: Yup.string().required('Required'),
+            password: Yup.string().required('Required'),
+            ...(isSignup && { phone: Yup.string().required('Required') }),
+        }),
+        onSubmit: async (values) => {
+            setError(null);
+            setLoading(true);
+            try {
+                const data = isSignup
+                    ? await handleSignup(values)
+                    : await handleSignin(values);
+
+                if (!isSignup) {
+                    setTokens(data.accessToken, data.refreshToken);
+                    router.push('/');
+                } else {
+                    console.log('Auth success:', data);
+                }
+            } catch (e) {
+                setError('Ошибка авторизации');
+            } finally {
+                setLoading(false);
+            }
+        },
+    });
 
     return (
-        <div className='container'>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <h2 className="text-xl font-semibold">Login</h2>
+        <form onSubmit={formik.handleSubmit} className="space-y-4 max-w-sm mx-auto mt-10 bg-white py-4 px-16 rounded-3xl min-w-[500px]">
+            <h2 className='mb-6 text-center font-semibold text-[28px]'>Login to your account</h2>
+            <div>
+                <label htmlFor="username" className="block text-sm font-medium mb-2">Username</label>
+                <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    autoComplete='username'
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    className="w-full px-3 py-2 border rounded"
+                />
+                {formik.touched.username && formik.errors.username && (
+                    <p className="text-red-500 text-sm">{formik.errors.username}</p>
+                )}
+            </div>
 
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="username" className="text-sm font-medium text-gray-700">Email</label>
+            <div>
+                <div className='flex items-center justify-between mb-3'>
+                    <label htmlFor="password" className="block text-sm font-medium">Password</label>
+                    {!isSignup && (<label><span className='text-[#19897b] cursor-pointer'>Forgot?</span></label>)}
+                </div>
+
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    className="w-full px-3 py-2 border rounded"
+                />
+                {formik.touched.password && formik.errors.password && (
+                    <p className="text-red-500 text-sm">{formik.errors.password}</p>
+                )}
+            </div>
+
+            {isSignup && (
+                <div>
+                    <label htmlFor="phone" className="block text-sm font-medium mb-2">Phone</label>
                     <input
-                        id="username"
+                        id="phone"
+                        name="phone"
                         type="text"
-                        placeholder="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        onChange={formik.handleChange}
+                        value={formik.values.phone}
+                        className="w-full px-3 py-2 border rounded"
                     />
+                    {formik.touched.phone && formik.errors.phone && (
+                        <p className="text-red-500 text-sm">{formik.errors.phone}</p>
+                    )}
                 </div>
+            )}
 
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="password" className="text-sm font-medium text-gray-700">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                </div>
+            {error && <p className="text-red-600 text-sm">{error}</p>}
 
-                <div className="text-sm text-right text-blue-600 hover:underline cursor-pointer">
-                    Forgot password?
-                </div>
+            <Button type='submit' disabled={loading} className='mt-4'>
+                {loading ? 'Loading...' : isSignup ? 'Sign Up' : 'Sign In'}
+            </Button>
 
-                <button
-                    type="submit"
-                    className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition"
-                >
-                    Sign In
-                </button>
-
-                <div className="text-sm text-center text-gray-600">
-                    Don’t have an account? <span className="text-blue-600 hover:underline cursor-pointer">Register for free</span>
-                </div>
-            </form>
-        </div>
+            <div className='w-full text-center'>
+                {!isSignup ? (
+                    <label>Don't have an account? <span className='text-[#19897b] cursor-pointer' onClick={() => setFormType('signup')}>Sign up</span></label>
+                ) : (
+                    <label>Already have an account? <span className='text-[#19897b] cursor-pointer' onClick={() => setFormType('signin')}>Log in</span></label>
+                )}
+            </div>
+        </form>
     );
 };
+
+export default AuthForm;
